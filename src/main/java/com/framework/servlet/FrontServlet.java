f9507fb1891dd9f9c3e3a5af1f5fb1bdb5fed1bd
miahyyy <sonyahrafa@gmail.com>
2025-09-22 21:11:56 +0300

    Update deploy,FrontServlet

--- src/main/java/com/framework/servlet/FrontServlet.java
+++ src/main/java/com/framework/servlet/FrontServlet.java
@@ -1,23 +1,32 @@
 package com.framework.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
 public class FrontServlet extends HttpServlet {
 
     @Override
-    public void service(HttpServletRequest request, HttpServletResponse response){
-        System.out.println(request.getRequestURL());
+    public void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        out.println("URL demandée: " + request.getRequestURL().toString());
+        System.out.println("URL demandée: " + request.getRequestURL().toString());
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         service(request, response);
     }
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-       service(request, response);
-    } 
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        service(request, response);
+    }
 }
