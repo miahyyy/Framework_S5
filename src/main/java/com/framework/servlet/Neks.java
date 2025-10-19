4f5a30e3b62856451507f66b4f50d25dd19b451a
miahyyy <sonyahrafa@gmail.com>
2025-10-19 10:59:21 +0300

    sprint2: annotation Neks

--- src/main/java/com/framework/servlet/Neks.java
+++ src/main/java/com/framework/servlet/Neks.java
@@ -1,12 +1,9 @@
 package com.framework.servlet;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
 public @interface Neks {
-    String url();
+    String value() default "";
 }
